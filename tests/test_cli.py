300c823e3a669418e6c0741af861705a5fadccbf
DeadNews <uhjnnn@gmail.com>
2022-07-13 10:39:51 +0200

    Fix test_click

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,4 +7,3 @@ from src.py_image_uploader.cli import main
 def test_click():
     runner = CliRunner()
     assert runner.invoke(cli=main, args=["--help"]).exit_code == 0
-    assert runner.invoke(cli=main, args=["--bbcode"]).exit_code == 0
